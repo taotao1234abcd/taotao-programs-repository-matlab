56442a663412ecb12bfc742d0beb3afbe76f219e
S.T. ZHANG <49369890+taotao1234abcd@users.noreply.github.com>
2019-04-12 00:17:02 +0800

    Update 按指定格式输出和对齐.m

--- 按指定格式输出和对齐.m
+++ 按指定格式输出和对齐.m
@@ -14,6 +14,15 @@ if display == 'y'
     if conv_flag == 0
         disp('                                      GENERATION             LOAD')
         disp('       BUS     VOLTS     ANGLE      REAL  REACTIVE      REAL  REACTIVE ')
+        
+        for m = 1:size(bus_sol,1)
+            for n = 1:size(bus_sol,2)
+                if abs(bus_sol(m,n)) < 1e-9
+                    bus_sol(m,n) = 0;
+                end
+            end
+        end
+        
         for i = 1:size(bus_sol,1)
             fprintf('%10d%10.4f%10.4f',bus_sol(i,1:3));
             
@@ -32,7 +41,7 @@ if display == 'y'
         end
         fprintf('\n');
         disp('                      LINE FLOWS                     ')
-        disp('      LINE  FROM BUS    TO BUS      REAL  REACTIVE   ')
+        disp('      LINE  FROM BUS    TO BUS      REAL  REACTIVE   ')  
         for i = 1:size(line_ffrom,1)
             fprintf('%10d%10d%10d%10.4f%10.4f\n',line_ffrom(i,1:5));
         end
@@ -41,3 +50,4 @@ if display == 'y'
         end
     end
 end
+
